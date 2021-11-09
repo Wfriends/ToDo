43020e3f16298e1e543c83464f0d4cfe2b971822
Sorokovsky <Sorokovskys@urk.net>
2021-11-09 19:43:51 +0300

    upgrade popub creating new progect

--- #src/js/newProgect.js
+++ #src/js/newProgect.js
@@ -1,8 +1,21 @@
-newProgect.addEventListener('click', showCreatePopub);
-function createProgect() {
-    newProgect.parentElement.insertAdjacentHTML('beforebegin', '<li class="sidebar__item"><a href="#" class="sidebar__link">jghgfggfgfgf</a></li>');
+newProgect.addEventListener('click', ()=>{showPopub(popubForCreatingProgect)});
+createForm.addEventListener('submit', createProgect);
+closeCP.addEventListener('click', ()=>{closePopub(popubForCreatingProgect)});
+function closePopub(popub){
+    popub.classList.remove('active');
+    document.body.classList.remove('lock');
 }
-function showCreatePopub() {
-    popubForCreatingProgect.classList.add('active');
+function clearForm (form){
+    form.reset();
+}
+function createProgect(e) {
+    e.preventDefault();
+    let name = createForm.children[0].value;
+    newProgect.parentElement.insertAdjacentHTML('beforebegin', `<li class="sidebar__item"><a href="#" class="sidebar__link">${name}</a></li>`);
+    closePopub(popubForCreatingProgect);
+    clearForm(createForm);
+}
+function showPopub(popub) {
+    popub.classList.add('active');
     document.body.classList.add('lock');
 }
