835cd5265931fdb11117e1aa1695f2ecd06ced59
Sorokovsky <Sorokovskys@urk.net>
2021-11-09 17:46:57 +0300

    function "createProgect"

--- #src/js/newProgect.js
+++ #src/js/newProgect.js
@@ -1,6 +1,14 @@
 newProgect.addEventListener('click', showCreatePopub);
-function createProgect() {
-    newProgect.parentElement.insertAdjacentHTML('beforebegin', '<li class="sidebar__item"><a href="#" class="sidebar__link">jghgfggfgfgf</a></li>');
+createForm.addEventListener('submit', createProgect);
+function closeCreatePopub(){
+    popubForCreatingProgect.classList.remove('active');
+    document.body.classList.remove('lock');
+}
+function createProgect(e) {
+    e.preventDefault();
+    let name = createForm.children[0].value;
+    newProgect.parentElement.insertAdjacentHTML('beforebegin', `<li class="sidebar__item"><a href="#" class="sidebar__link">${name}</a></li>`);
+    closeCreatePopub();
 }
 function showCreatePopub() {
     popubForCreatingProgect.classList.add('active');
