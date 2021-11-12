056a8376e6bb19d7f292b332c1ed9c5ada277750
Sorokovsky <Sorokovskys@ukr.net>
2021-11-12 21:26:10 +0300

    node

--- #src/js/index.js
+++ #src/js/index.js
@@ -3,7 +3,7 @@ let mysql = require('mysql2');
 let conn = mysql.createConnection(config);
 conn.connect(function(err){
     if (err) {
-        console.log(`У підключенні до бази данних сталася помилка: ${err.message}`);
+        console.error(`У підключенні до бази данних сталася помилка: ${err.message}`);
     }else{
         console.log('Підключення до бази данних успішне');
     }
---
+++ ToDo/js/variables.js
@@ -0,0 +1,4 @@
+let header = document.querySelector('.header'),
+    sidebar = document.querySelector('.sidebar'),
+    main = document.querySelector('.main'),
+    headerHeight = header.getC
