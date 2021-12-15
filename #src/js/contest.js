602926094ab1234621d8799b38ba1e86dd411b0b
Sorokovsky <Sorokovskys@ukr.net>
2021-12-15 18:14:30 +0300

    close context

--- #src/js/contest.js
+++ #src/js/contest.js
@@ -3,11 +3,26 @@ tableItem.forEach(item => {
         let points = item.children[i].children[0];
         if (points != undefined){
             points = points.children[1];
-            points.addEventListener('click', function (e){
-                e.preventDefault();
-                let popub = points.children[3]
-                console.log(popub);
-                showPopub(popub)
+            console.log(points);
+            document.body.addEventListener('click', (e) => {
+                if(!e.target.classList.contains('task__context _active') && !e.target.classList.contains('task__points')){
+
+                    closePopub(points.children[3])
+                }
+            });
+            document.body.addEventListener('click', function (e){
+                if(e.target.classList.contains('task__points') || e.target.classList.contains('task__bulet')){
+                    e.preventDefault();
+                    let popub;
+                    if (e.target.classList.contains('task__points')) {
+                        popub = e.target.children[3];
+                    }
+                    if(e.target.classList.contains('task__bulet')){
+                        popub = e.target.parentElement.children[3];
+                    }
+                    console.log(popub);
+                    showPopub(popub);
+                }
             });
         }
     }
